dd5eef976d83986a74ee3369524abd841deeb0d2
omar <oa2300487@tkh.edu.eg>
2025-11-10 13:08:03 +0200

    week 8 dr noor cookie

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TripRouter=require('./routes/TripRoutes');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRouter = require('./routes/authRoutes.js');
-
+const userRouter = require('./routes/UserRouter.js');
 // const UserRouter=require('./routes/UserRouter');
 dotenv.config();
 app.use(cors());
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/trips',TripRouter);
+
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 app.use('auth', authRouter);
 // app.use('/apl/v1/users',UserRouter);
